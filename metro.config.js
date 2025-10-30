82b55fb9cf08c6ec1ee2ec53ce1e4c3e2c64488c
felipelee <felipelee252@gmail.com>
2025-10-30 01:04:09 -0600

    Updated metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -4,4 +4,17 @@ const { getSentryExpoConfig } = require("@sentry/react-native/metro");
 // eslint-disable-next-line no-undef
 const config = getSentryExpoConfig(__dirname);
 
+config.resolver = {
+  ...config.resolver,
+  assetExts: [
+    ...(config.resolver?.assetExts || []),
+    'png',
+    'jpg',
+    'jpeg',
+    'gif',
+    'webp',
+    'svg',
+  ],
+};
+
 module.exports = withNativeWind(config, { input: "./global.css" });
